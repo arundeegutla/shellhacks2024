0c153e063a4c8ecad1ede1f6fbacd4223d836f62
Sachin S <ssk4988@gmail.com>
2024-09-28 12:00:03 -0400

    add hello, world to backend

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,19 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
+import { onCall, CallableRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+
+// Define the type for the data parameter
+interface HelloWorldData {
+    name?: string;
+}
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
+export const helloWorld = onCall((request: CallableRequest<HelloWorldData>) => {
+    logger.info("Hello logs!", { structuredData: true });
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    // Access data sent by the client
+    const name = request.data.name || "World";
+
+    // Return a response
+    return { message: `Hello, ${name}!` };
+});
