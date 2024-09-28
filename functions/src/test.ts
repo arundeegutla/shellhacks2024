22dc1059d1dbdb41f59c552e46611a2da311d0a4
Chris G. <38964705+Silver-Sword@users.noreply.github.com>
2024-09-28 19:34:52 -0400

    create, update, and read both rounds and game boards (#11)

--- functions/src/test.ts
+++ functions/src/test.ts
@@ -1,9 +1,23 @@
 import { logger } from "firebase-functions/v2";
-import { createRound, getGameBoard } from "./firebase-utils/firebaseCalls";
+import { createRound, getGameBoard, getTrueWord, setTrueWord } from "./firebase-utils/firebaseCalls";
+import { guessWord } from "./word-utils.ts/wordGuessing";
 
+function assert(condition: boolean, msg?: string)
+{
+    if(!condition)
+    {
+        throw Error(msg);
+    }
+}
 export async function runTest()
 {
-    await createRound(["a", "b", "c"], "0", "room0101", 6, 5);
-    const game = await getGameBoard("b", "0", "room0101");
+    const roomId = "ABCEDF", roundId = "1", userId = "a", trueWord = "guess";
+    await createRound([userId, "b", "c"], roundId, roomId, 6, 5);
+    await setTrueWord(trueWord, roundId, roomId);
+
+    await guessWord("crane", userId, roundId, roomId);
+    const game = await getGameBoard(userId, roundId, roomId);
     logger.log(`game board: ${JSON.stringify(game)}`);
+
+    assert(trueWord === await getTrueWord(roundId, roomId), "got incorrect true word");
 }
