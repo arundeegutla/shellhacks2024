0c9669b7224c1f6dcd54cd3d38d882db4f32c318
Arun Deegutla <arundeegutla2004@gmail.com>
2024-09-28 13:04:09 -0400

    room query added

--- frontend/app/room/[roomId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button, IconButton } from "@mui/material";
-import Link from "next/link";
-
-export default function Room() {
-  return (
-    <div className="">
-    </div>
-  );
-}
--- frontend/app/room/page.tsx
+++ frontend/app/room/page.tsx
@@ -1,5 +1,31 @@
+"use client";
+import { useParams, useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+import Link from 'next/link';
+
 export default function Room() {
+    const router = useRouter();
+    const searchParams = useSearchParams()
+    const roomId = searchParams.get('id');
+
+  useEffect(() => {
+    if (!roomId) {
+      router.push('/');
+    }
+  }, [roomId, router]);
+
+  if (!roomId) {
     return (
-        <p>Simple Room</p>
-    )
+      <div>
+        <p>Room ID is missing, redirecting...</p>
+        <Link href="/" className="text-blue-500 underline">Go to Home</Link>
+      </div>
+    );
+  }
+
+  return (
+    <div className="m-auto">
+      Your Room ID is: <span className="font-bold">{roomId}</span>
+    </div>
+  );
 }
