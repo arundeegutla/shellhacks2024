e26e7c99f4945858c5c27c267540755ddd189447
Arun Deegutla <arundeegutla2004@gmail.com>
2024-09-28 04:32:34 -0400

    made next static

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    distDir: 'out', // Where to export all pages
+    staticPageGenerationTimeout: 1000,
+    reactStrictMode: false
+}
 
 export default nextConfig;
