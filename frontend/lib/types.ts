833af9965229061ca79e5f27ddde065a3dce5c82
Sachin S <ssk4988@gmail.com>
2024-09-28 22:20:30 -0400

    add word dictionary

---
+++ frontend/lib/types.ts
@@ -0,0 +1,82 @@
+import { ErrorCode } from "./util"
+
+export interface MakeRoomResponse {
+    roomCode: string,
+    error: ErrorCode
+}
+
+export interface JoinRoomResponse {
+    error: ErrorCode,
+    userID: string,
+    roomListener: string
+}
+
+
+export interface UserType {
+    name: string,
+    userID: string,
+    playerID: number,
+    roomCode: string
+};
+
+
+
+
+export enum Verdict
+{
+    BLANK=0,
+    ABSENT=1,
+    PRESENT=2,
+    CORRECT=3,
+};
+
+export type Row =
+{
+    verdicts: Verdict[],
+    guess?: string,
+};
+
+export type GameBoard =
+{
+    rows: Row[],
+    guesses_left: number, 
+    
+    // game constants
+    num_guesses: number,
+    word_length: number,
+    time_started: number,
+    true_word?: string,
+};
+
+export type Round =
+{
+    has_started: boolean,
+    time_started: number,
+    true_word: string,
+    num_guesses_allowed: number,
+    word_length: number,
+    games: { [key: string]: GameBoard },
+};
+
+export interface RoomType {
+    gameID: -1,
+	users: UserType[],
+	open: true,
+	roomCode: string,
+	listenDocumentID: string,
+	roundStarted: boolean,
+	roundCount: number
+};
+
+export interface GetGameInfoResponse {
+    error: ErrorCode,
+    roomData: 
+}
+
+export interface GetRoomInfoResponse {
+    roomListener: string,
+    usersInRoom: string[],
+    requesterIsHost: boolean
+    host: string,
+    error: ErrorCode
+}
