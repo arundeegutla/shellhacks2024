d8d8476458cd18eef84018efe8953e1a592b23dd
Arun Deegutla <arundeegutla2004@gmail.com>
2024-09-29 07:21:37 -0400

    leaderboard integration

--- frontend/app/room/page.tsx
+++ frontend/app/room/page.tsx
@@ -6,7 +6,7 @@ import { ErrorCode, getNameHelperText, validateName } from '@/lib/util';
 import { db, getGameInfo, getRoomInfo, submitGuess, submitSecretWord } from '@/lib/firebase';
 import { RoomType } from '@/lib/types';
 import { doc, onSnapshot } from 'firebase/firestore';
-import Leaderboard, { Player } from '@/components/Leaderboard';
+import Leaderboard from '@/components/Leaderboard';
 import Link from 'next/link';
 import MiniWordle from '@/components/MiniWordle';
 import Loading from '@/components/Loading';
@@ -28,7 +28,10 @@ export default function Room() {
   const [room, setRoom] = useState<RoomType | null>(null);
   const [isHost, setIsHost] = useState<boolean>(false);
   const [pickWord, setPickWord] = useState<string>('');
+  const [pickWordBool, setPickWordBool] = useState<boolean>(false);
   const [guesses, setGuesses] = useState<string[]>(Array(MAX_GUESSES).fill(''));
+  const [start, setStart] = useState(false);
+  const [hostName, setHostName] = useState('');
 
   const wordInputRef = useRef<HTMLInputElement>(null)
   const validateWord = (s: string) => {
@@ -117,7 +120,9 @@ export default function Room() {
       setGuesses(newGuesses);
     }
     setIsHost(roomData.hostID === userID);
+    setHostName(roomData.users.find(user => user.userID === roomData.hostID)?.name || '');
     setRoom(roomData);
+    setStart(currentRound.has_started)
     setLoading(false);
   };
 
@@ -130,7 +135,7 @@ export default function Room() {
   useEffect(() => {
     let timer: NodeJS.Timeout;
 
-    if (timeLeft > 0 && !gameOver) {
+    if (start && timeLeft > 0 && !gameOver) {
       timer = setInterval(() => {
         setTimeLeft(prev => prev - 1);
       }, 1000);
@@ -139,7 +144,7 @@ export default function Room() {
     }
 
     return () => clearInterval(timer);
-  }, [timeLeft, gameOver]);
+  }, [timeLeft, gameOver, start]);
 
 
   if (loading || !room) {
@@ -169,6 +174,15 @@ export default function Room() {
     }
   };
 
+  const submitPickedWord = async () => {
+    const obj = { room_code: roomId, word: pickWord.toUpperCase(), user_id: userID!, round_id: (roundNum - 1).toString() };
+    console.log("submitting secret word", obj);
+    await submitSecretWord(obj).then((resp) => {
+      setPickWordBool(true);
+      console.log("secret word response", resp);
+    });
+  }
+
 
   const roundNum = room!.roundCount;
   const currentRound = room!.rounds[roundNum - 1];
@@ -177,8 +191,7 @@ export default function Room() {
     <div className="m-auto w-full">
       <div className="flex flex-row items-center p-4 w-full">
         <div className='flex flex-row items-end justify-end pr-16 w-[33%]'>
-
-          {/* TODO <Leaderboard players={ } /> */}
+          <Leaderboard players={room} />
         </div>
         <div className='flex flex-col items-center justify-center wordle w-[34%]'>
           <div className='flex flex-row items-center justify-center rounded-lg bg-white/10 px-2 py-1 mb-6 font-medium  min-w-20 gap-1'>
@@ -186,10 +199,11 @@ export default function Room() {
             {Math.floor(timeLeft / 60)}:{(timeLeft % 60).toString().padStart(2, '0')}
           </div>
           {isHost &&
-            <>
-              <div className="mb-4 w-64 relative w">
-                <a className="text-sm">Name</a>
+            <div className='bg-white/5 p-10 flex flex-col items-center justify-center rounded-lg'>
+              <div className="mb-4 w-full">
+                <h2 className="text-xl text-center mb-2">{!pickWordBool ? 'Pick a word!' : 'Ooh nice one!'}</h2>
                 <input
+                  disabled={pickWordBool}
                   ref={wordInputRef}
                   type="text"
                   id="name-input"
@@ -197,25 +211,23 @@ export default function Room() {
                   onChange={changeName}
                   className={`w-full px-4 py-2 bg-white/15 rounded-lg text-white placeholder-white/50 focus:outline-none focus:ring-2 focus:ring-blue-500 ${!validateWord(pickWord) ? 'border-red-500' : ''
                     }`}
-                  placeholder="Enter a Name"
+                  placeholder="5 letter word"
                   required
                   aria-invalid={!validateWord(pickWord)}
                   aria-describedby="name-error"
                 />
               </div>
-              <button className={`px-6 py-2 rounded-lg shadow-lg font-bold transition-colors ${validateWord(pickWord)
+              <button className={`px-6 py-2 rounded-lg shadow-lg font-bold transition-colors ${!pickWordBool && validateWord(pickWord)
                 ? 'bg-green-500 text-white hover:bg-green-600'
                 : 'bg-gray-300 text-gray-500 cursor-not-allowed'
                 }`}
-                disabled={!validateWord(pickWord)} onClick={async () => {
-                  const obj = { room_code: roomId, word: pickWord.toUpperCase(), user_id: userID!, round_id: (roundNum - 1).toString() };
-                  console.log("submitting secret word", obj);
-                  const response = (await submitSecretWord(obj)).data;
-                  console.log("secret word response", response);
-                }}>Submit</button>
-            </>
+                disabled={pickWordBool || !validateWord(pickWord)} onClick={submitPickedWord}>Submit</button>
+            </div>
           }
+          {!isHost && !start && <h1 className='text-xl mb-5'>{hostName} is picking the word...</h1>}
+          {!isHost && start && <h1 className='text-xl mb-5'>{hostName} is picked the word!</h1>}
           {!isHost && <LiveBoard submit={submitSolve} guesses={guesses} gameOver={gameOver} setGameOver={setGameOver} solution={currentRound.true_word} />}
+
         </div>
         <div className='flex flex-col gap-1 mr-auto w-[33%] pl-16'>
           {others.map((player, i) => {
--- frontend/components/Leaderboard.tsx
+++ frontend/components/Leaderboard.tsx
@@ -1,18 +1,14 @@
+import { RoomType } from "@/lib/types";
 import { GiTrophyCup } from "react-icons/gi";
 
-export type Player = {
-  score: number;
-  name: string;
-  isHost: boolean;
-}
 
-export default function Leaderboard({ players }: { players: Player[] }) {
+export default function Leaderboard({ players }: { players: RoomType }) {
 
-  players.sort((a, b) => b.score - a.score);
+  players.users.sort((a, b) => b.points - a.points);
 
   return (
     <ul className="rounded-xl bg-zinc-800 overflow-hidden w-fit">
-      {players.map((profile, idx) => (
+      {players.users.map((profile, idx) => (
         <li key={idx} className={`py-3 px-2 ${idx % 2 == 0 ? 'bg-black/30' : 'bg-white/5'}`}>
           <div className="flex items-center space-x-4">
             <div style={{ width: "20px" }} className="flex-shrink-0">
@@ -27,7 +23,7 @@ export default function Leaderboard({ players }: { players: Player[] }) {
             </div>
             <div className='flex flex-row items-center text-amber-500 my-blur transparent-dark rounded-md px-3 py-1 text-base font-medium'>
               <div style={{ fontSize: "20px" }}><GiTrophyCup /></div>
-              <h3 className='m-1'>{profile.score}</h3>
+              <h3 className='m-1'>{profile.points}</h3>
             </div>
           </div>
         </li>
